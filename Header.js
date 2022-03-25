6b3a255d9b6f348f6282c65ea40b5f211bf43b4b
kpagariya <38912672+kpagariya@users.noreply.github.com>
2022-03-25 16:13:44 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 function Header(){
 	return (
-		<div>
-			<h1> My Contact Manager </h1>
+		<nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+		<div className="container">
+			<a href="/" className="navbar-brand">My Contact Manager</a>
 		</div>
+		<div>
+            <ul className="navbar-nav mr-auto">
+	 			<li className="nav-item"> <a href="/" className="nav-link">Login</a></li>	
+			</ul>
+		</div>
+	</nav>
 	);
 }
 export default Header;
